86202b8e7bac93828303d29228bec90297d0e46b
Luciano Zembruzki <40046862+zembruzkill@users.noreply.github.com>
2024-01-08 13:19:12 -0300

    trabalhe conosco

---
+++ src/app/trabalhe-conosco/page.tsx
@@ -0,0 +1,17 @@
+import Header from '@/components/layout/headers/Header'
+
+export default function TrabalheConosco() {
+    return (
+        <>
+            <div className="min-h-screen bg-blackground items-center justify-center">
+                <Header />
+                <div className="w-full flex justify-center items-center pt-32">
+                    <h1 className="text-white text-title-xxl">
+                        Trabalhe Conosco
+                    </h1>
+                </div>
+            </div>
+            <div className="bg-black"></div>
+        </>
+    )
+}
--- src/components/layout/headers/Header.tsx
+++ src/components/layout/headers/Header.tsx
@@ -101,6 +101,14 @@ export default function Header() {
                                     CONTATO
                                 </Link>
                             </li>
+                            <li>
+                                <Link
+                                    href="/trabalhe-conosco"
+                                    className="block py-2 pr-4 pl-3 text-white rounded  "
+                                >
+                                    TRABALHE CONOSCO
+                                </Link>
+                            </li>
                         </ul>
                     </div>
                 </div>
