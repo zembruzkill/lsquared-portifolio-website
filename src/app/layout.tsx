73aacfffba05c22d74037f78d1147bafbfb518ce
Luciano Zembruzki <40046862+zembruzkill@users.noreply.github.com>
2024-01-21 17:23:25 -0300

    slogan

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,21 +5,21 @@ import { Readex_Pro } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
 import Providers from '@/components/Providers'
 
-const roboto = Readex_Pro({ subsets: ['latin'], weight: ['200', '300', '400', '500', '600', '700'] })
+const roboto = Readex_Pro({
+    subsets: ['latin'],
+    weight: ['200', '300', '400', '500', '600', '700'],
+})
 
 export const metadata: Metadata = {
     title: 'L-Squared Digital Solutions',
-    description: 'A L-Squared Digital Solutions é uma empresa de desenvolvimento de software e marketing.',
+    description: 'Conectando a solução ideal pro seu negócio',
 }
 
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode
-    })
-{
-    
-    
+}) {
     return (
         <html lang="en">
             <body className={roboto.className}>
