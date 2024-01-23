9a8171f409bd590b67234f4cea76e2bcf07f46c1
Luciano Zembruzki <40046862+zembruzkill@users.noreply.github.com>
2024-01-23 16:47:10 -0300

    removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -108,7 +108,7 @@ export default function Home() {
             </div>
             <AboutComponent textColor="black" />
             <OurServicesComponent />
-            <StartAProject />
+            {/* <StartAProject /> */}
             <Footer />
         </>
     )
